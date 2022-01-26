b27c252ff5cb7ec0aef272d405e5c69a9155c479
aldrinburgos <burgos.aldrin@gmail.com>
2022-01-25 22:01:06 -0600

    set up tasks array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
+//display current day on header
 var currentDay = document.querySelector("#currentDay");
 currentDay.innerHTML = moment().format("MMMM Do, YYYY");
+
+//array for task time and description
+var savedTasks = [];
+var pm = 1;
+
+if (savedTasks.length === 0) {
+  for (var i = 9; i < 18; i++) {
+    if (i < 13) {
+      var tasks = { time: i + ":00 AM", task: "" };
+    } else {
+      var tasks = { time: pm++ + ":00 PM", task: "" };
+    }
+    savedTasks.push(tasks);
+  }
+} else {
+  //enter loadTask() logic here
+  console.log("saved");
+}
+console.log(savedTasks);
